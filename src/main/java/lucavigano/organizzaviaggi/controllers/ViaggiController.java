581f433f199d065f9f72164b728002aa02a07ea2
luca-vigano <luca.vigano@hotmail.com>
2024-11-01 16:19:47 +0100

    viaggio controller

---
+++ src/main/java/lucavigano/organizzaviaggi/controllers/ViaggiController.java
@@ -0,0 +1,63 @@
+package lucavigano.organizzaviaggi.controllers;
+
+import lucavigano.organizzaviaggi.entities.Viaggio;
+import lucavigano.organizzaviaggi.exception.BadRequestException;
+import lucavigano.organizzaviaggi.payloads.ViaggioDTO;
+import lucavigano.organizzaviaggi.services.ViaggioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/viaggi")
+
+public class ViaggiController {
+    @Autowired
+    private ViaggioService viaggioService;
+
+    @GetMapping
+    public Page<Viaggio> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size,
+                                 @RequestParam(defaultValue = "id") String sortBy) {
+        return this.viaggioService.findAll(page, size, sortBy);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Viaggio save(@RequestBody @Validated ViaggioDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            String message = validationResult.getAllErrors().stream().map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload! " + message);
+        }
+
+        return this.viaggioService.save(body);
+    }
+
+    @GetMapping("/{viaggioId}")
+    public Viaggio findById(@PathVariable UUID viaggioId) {
+        return this.viaggioService.findById(viaggioId);
+    }
+
+    @PutMapping("/{viaggioId}")
+    public Viaggio findByIdAndUpdate(@PathVariable UUID viaggioId, @RequestBody @Validated ViaggioDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            validationResult.getAllErrors().forEach(System.out::println);
+            throw new BadRequestException("Errore nel payload!");
+        }
+        return this.viaggioService.findByIdAndUpdate(viaggioId, body);
+    }
+
+    @DeleteMapping("/{viaggioId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID viaggioId) {
+        this.viaggioService.findByIdAndDelete(viaggioId);
+    }
+
+
+}
--- src/main/java/lucavigano/organizzaviaggi/payloads/ViaggioDTO.java
+++ src/main/java/lucavigano/organizzaviaggi/payloads/ViaggioDTO.java
@@ -2,6 +2,7 @@ package lucavigano.organizzaviaggi.payloads;
 
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lucavigano.organizzaviaggi.entities.Dipendente;
 
@@ -12,11 +13,10 @@ public record ViaggioDTO(
         @NotEmpty(message = "La destinazione è obbligatoria!")
         @Size(min = 2, max = 40, message = "La destinazione deve essere compreso tra 2 e 40 caratteri!")
         String destinazione,
-        @NotEmpty(message = "La data del viaggio è obbligatoria!")
+        @NotNull(message = "La data del viaggio è obbligatoria!")
         LocalDate data_viaggio,
         @NotEmpty(message = "Lo stato del viaggio è obbligatorio!")
         @Size(min = 2, max = 40, message = "Lo stato del deve essere compreso tra 2 e 40 caratteri!")
         String stato,
-        @NotEmpty(message = "Il dipendente è obbligatorio!")
         Dipendente dipendente) {
 }
